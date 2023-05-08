8e08909dc2200dc8aaae33af924113eda99321e2
KeenBash <52461996+KeenBash@users.noreply.github.com>
2023-05-08 13:43:59 +0000

    Deploying to gh-pages from @ KeenBash/KeenBash.github.io@f32ef12a503929d7b975c3e9feb1679539b0d22e 🚀

---
+++ assets/pageview-59d0bb38.js
@@ -0,0 +1,1 @@
+import{i as e}from"./app-3c42b8f2.js";import"./framework-945e1ba3.js";const a=()=>e({serverURL:"https://waline.keeend.eu.org"});export{a as updatePageview};
--- assets/pageview-a42fd8bb.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./app-555fa643.js";import"./framework-945e1ba3.js";const a=()=>e({serverURL:"https://waline.keeend.eu.org"});export{a as updatePageview};
